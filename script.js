c5ef60dfe07642dd02f188efc628f2cb3508edf3
toth-marcell <toth.marcell.21i@egressy.info>
2025-09-09 07:58:13 +0200

    first task

---
+++ script.js
@@ -0,0 +1,7 @@
+function login() {
+  const nameField = document.getElementById("nameField");
+  const passwordField = document.getElementById("passwordField");
+  nameField.value = "";
+  passwordField.value = "";
+  alert("Sikeres bejelentkezés!");
+}
